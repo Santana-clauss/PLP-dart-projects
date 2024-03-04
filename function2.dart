b290301f45edbb6731e599843631cf79c3b99dfe
santana clauss <jepchumbasantana@gmail.com>
2024-03-04 09:38:45 +0300

    added getFirstElement func

--- function2.dart
+++ function2.dart
@@ -18,6 +18,11 @@ int stringLength(String a) {
    return a.length;
 }
 
+String getFirstElement(){
+  List<String>names=["santana","mercy","faith"];
+  return names[0];
+}
+
 void main(){
   
   print(addTwo(2, 6));
@@ -25,5 +30,6 @@ void main(){
   print(multiplyTwo(20, 30));
   print(divideTwo(12, 5));
   print(stringLength("santana"));
+  print(getFirstElement());
 
 }
