dd867431e1fe0f467ba57aae44a79324f6dd37cf
santana clauss <jepchumbasantana@gmail.com>
2024-03-21 15:33:56 +0300

    inheritance

--- controls.dart
+++ controls.dart
@@ -1,37 +1,34 @@
 import 'dart:io';
-void myFunc(){
-  var list1=[2,8,9,10];
+
+void myFunc() {
+  var list1 = [2, 8, 9, 10];
   for (var i in list1) {
     print("Element $i");
-    
   }
 }
 
-void myFunc2(){
+void myFunc2() {
   String? username;
   String? password;
-  while (username == null || password == null) 
-  {
+  while (username == null || password == null) {
     stdout.write("Enter your username: ");
     username = stdin.readLineSync();
     stdout.write("Enter your password: ");
     password = stdin.readLineSync();
-    if(username==null&&password==null)
-    {
+    if (username == null && password == null) {
       print("Please enter your username and password");
     }
-   
   }
- print("Hello $username you can proceed to login ");
+  print("Hello $username you can proceed to login ");
 }
 
 void main() {
-  stdout.write("Enter a number: "); 
+  stdout.write("Enter a number: ");
   String? input = stdin.readLineSync();
 
   if (input != null) {
     try {
-      int num = int.parse(input); 
+      int num = int.parse(input);
       if (num % 2 == 0) {
         print("The number $num is even.");
       } else {
---
+++ course_modules/wk5 projects/inheritance.dart
@@ -0,0 +1,29 @@
+
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, this.age);
+
+  void displaydetails() {
+    print('Hello, my name is $name and I am $age years old.');
+  }
+}
+
+class Student extends Person {
+  String grade;
+  String school;
+
+  Student(String name, int age, this.grade, this.school) : super(name, age);
+
+  void displaydetails() {
+    super.displaydetails(); 
+    print('I am a student at $school.');
+  }
+}
+
+void main() {
+  
+  Student student1= Student('John Doe', 16, '10th', "PLP academy");
+  student1.displaydetails();
+}
