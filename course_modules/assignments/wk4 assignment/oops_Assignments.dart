67d6c7b21aadbc63008f7337dc3bceb34463eb9a
santana clauss <jepchumbasantana@gmail.com>
2024-03-22 17:47:16 +0300

    added oops assignment

---
+++ course_modules/assignments/wk4 assignment/oops_Assignments.dart
@@ -0,0 +1,44 @@
+class Student {
+  String name;
+  int age;
+  int gradeLevel;
+
+  Student(this.name, this.age, this.gradeLevel);
+
+  void printInfo() {
+    print('Student Information:');
+    print('Name: $name');
+    print('Age: $age');
+    print('Grade Level: $gradeLevel');
+  }
+}
+
+class Teacher {
+  String name;
+  int age;
+  String subject;
+
+  Teacher(this.name, this.age, this.subject);
+
+  void printInfo() {
+    print('Teacher Information:');
+    print('Name: $name');
+    print('Age: $age');
+    print('Subject: $subject');
+  }
+}
+
+class School {
+  void createObjectsAndPrintInfo() {
+    var student = Student('John', 15, 10);
+    var teacher = Teacher('Ms. Smith', 35, 'Mathematics');
+
+    student.printInfo();
+    teacher.printInfo();
+  }
+}
+
+void main() {
+  var school = School();
+  school.createObjectsAndPrintInfo();
+}
--- course_modules/wk5 projects/inheritance.dart
+++ course_modules/wk5 projects/inheritance.dart
@@ -2,7 +2,6 @@
 class Person {
   String name;
   int age;
-
   Person(this.name, this.age);
 
   void displaydetails() {
