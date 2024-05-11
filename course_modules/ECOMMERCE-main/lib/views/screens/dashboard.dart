f02e749914837fc847616effae3b570b4930c37f
santana clauss <jepchumbasantana@gmail.com>
2024-05-11 21:38:39 +0300

    updated

---
+++ course_modules/ECOMMERCE-main/lib/views/screens/dashboard.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            
+          )
+        ],
+      ),
+    );
+  }
+}
